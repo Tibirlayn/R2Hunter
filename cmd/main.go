6f4a1eb73d29041a301f7748ff0dfdee1fd82c55
Tibirlayn <tibirlayn@gmail.com>
2024-05-17 01:16:37 +0300

    create app

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Tibirlayn/R2Hunter/internal/config"
 	"github.com/Tibirlayn/R2Hunter/internal/logger"
+	"github.com/Tibirlayn/R2Hunter/internal/logger/sl"
 	"github.com/Tibirlayn/R2Hunter/storage/mssql"
+
 )
 
 func main() {
@@ -13,18 +16,30 @@ func main() {
 	cfg, cfgdb := config.MustLoad()
 
 	// инициализировать логгер: log/slog
-	loggers := logger.SetupLogger(cfg.Env)
-	loggers.Info("starting application")
+	log := logger.SetupLogger(cfg.Env)
+	log.Info("starting application")
 
 	// инициализировать СУБД: MS SQL
 	storage, err := mssql.New(cfgdb)
 	if err != nil {
-		panic(err)
+		log.Error("faild to init storage", sl.Err(err))
+		os.Exit(1)
 	}
 
 	fmt.Println(storage)
 
 	// инициализировать роутер: chi
+/* 	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+ 	router.Use(mwLogger.New(log))  логирование запросов подробнее узнать github.com/Tibirlayn/R2Hunter/internal/http-server/middleware/logger
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+ */
+
+	// инициализировать роутер: fiber
+	
 
 	// инициализировать приложение (app):
 }
--- go.mod
+++ go.mod
@@ -5,19 +5,25 @@ go 1.21.4
 require gorm.io/gorm v1.25.10
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/microsoft/go-mssqldb v1.6.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/fatih/color v1.17.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/sqlserver v1.5.3
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
